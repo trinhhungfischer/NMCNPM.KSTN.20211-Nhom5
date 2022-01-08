cca45a57805fb36e14030bd6271b4c401b62e1ce
datcn1212 <84216034+datcn1212@users.noreply.github.com>
2022-01-08 14:20:11 +0700

    update bean

--- src/Bean/XetNghiemBean.java
+++ src/Bean/XetNghiemBean.java
@@ -2,21 +2,28 @@ package Bean;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import Bean.NhanKhauBean;
 import models.XetNghiemModel;
+import models.ChungMinhThuModel;
 
 public class XetNghiemBean {
     private XetNghiemModel xetNghiemModel;
+    private NhanKhauBean nhanKhauBean;
+    private ChungMinhThuModel chungMinhThuModel;
     private List<XetNghiemModel> listXetNghiemModels;
 
-    public XetNghiemBean(XetNghiemModel xetNghiemModel, List<XetNghiemModel> listXetNghiemModels) {
+    public XetNghiemBean(XetNghiemModel xetNghiemModel, List<XetNghiemModel> listXetNghiemModels, NhanKhauBean nhanKhauBean, ChungMinhThuModel chungMinhThuModel) {
         this.xetNghiemModel = xetNghiemModel;
         this.listXetNghiemModels = listXetNghiemModels;
+        this.nhanKhauBean = nhanKhauBean;
+        this.chungMinhThuModel = chungMinhThuModel;
     }
     
     public XetNghiemBean() {
         this.xetNghiemModel = new XetNghiemModel();
         this.listXetNghiemModels = new ArrayList<>();
+        this.nhanKhauBean = new NhanKhauBean();
+        this.chungMinhThuModel = new ChungMinhThuModel();
     }
 
     public XetNghiemModel getXetNghiemModel() {
@@ -43,17 +50,22 @@ public class XetNghiemBean {
     public String toString() {
         String res = "<html> <style>p {padding: 5px; margin-left: 20px} table, th, td {border: 1px solid black; border-collapse: collapse;} table {width: 500px}</style> <div>"
                 + "<h3>Thông tin cơ bản"
-                + "<p>Mã xét nghiệm: <b>" + xetNghiemModel.getXetNghiemID()+ "</p>"
-                + "<p>Mã nhân khẩu: <b>" + xetNghiemModel.getNhanKhauID() + "</p>"
-                + "<p>Ngày xét nghiệm: <b>" + xetNghiemModel.getNgayXetNghiem().toString() + "</p>"
-                + "<p>Nơi xét nghiệm: <b>" + xetNghiemModel.getNoiXetNghiem() + "</p>"
-                + "<p>Nơi xét nghiệm: <b>" + xetNghiemModel.getHinhThucXetNghiem() + "</p>"
-                + "<p>Nơi xét nghiệm: <b>" + xetNghiemModel.getKetQuaXetNghiem() + "</p>";
+                + "<p>Mã nhân khẩu: <b>" + nhanKhauBean.getNhanKhauModel().getID()+ "</p>"
+                + "<p>Ngày sinh: <b>" + nhanKhauBean.getNhanKhauModel().getNamSinh()+ "</p>"
+                + "<p>Số CCCD: <b>" + nhanKhauBean.getChungMinhThuModel().getSoCMT()+ "</p>"
+                + "<p>Địa chỉ: <b>" + nhanKhauBean.getNhanKhauModel().getDiaChiHienNay()+ "</p>"
+                + "<h4>Lịch sử xét nghiệm<table>"
+                + "<tr>"
+                + "<th>Mã xét nghiệm:</th>"
+                + "<th>Ngày xét nghiệm:</th>"
+                + "<th>Nơi xét nghiệm:</th>"
+                + "<th>Hình thức xét nghiệm:</th>"
+                + "<th>Kết quả xét nghiệm:</th>";
             
         for (int i = 0; i < listXetNghiemModels.size(); i++) {
             res += "<tr>"
                     + "<td>"
-                    + listXetNghiemModels.get(i).getNhanKhauID()
+                    + listXetNghiemModels.get(i).getXetNghiemID()
                     + "</td>"
                     + "<td>"
                     + listXetNghiemModels.get(i).getNgayXetNghiem().toString()
